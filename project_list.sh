64fe85714b6195ea67cad1f2a863b3092508559b
damilola agboola <damilolaagboola2021@gmail.com>
2022-02-22 01:30:28 +0000

    project list created

---
+++ project_list.sh
@@ -0,0 +1,13 @@
+1. Create organisation when necessary 
+         
+
+2. Create teams and add members and assign roles & responsilities
+          
+
+ add users to team and grant required access
+
+3. Create repository for each project and share them with developers    
+          
+    
+4. We create an enabling and secured environment for developers to code.
+
